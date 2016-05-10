c94c7154c26befefd26c6b0df869dbc7b987fd97
Bradley C. Kuszmaul <bradley@mit.edu>
2016-05-10 17:47:46 +0000

    Make printtool compile

--- test/csi/toolkit/print_tool.cpp
+++ test/csi/toolkit/print_tool.cpp
@@ -13,8 +13,8 @@ void destroy() {
 extern "C" {
 
 // void __csi_init(csi_info_t info) {
-void __csi_init(uint32_t num_modules) {
-    fprintf(stderr, "Initialize tool, %d modules\n", num_modules);
+void __csi_init(const char * const name) {
+    fprintf(stderr, "Initialize tool, name=%s\n", name);
     atexit(destroy);
 }
 
@@ -23,31 +23,60 @@ void __csi_module_init(uint32_t module_id, uint64_t num_basic_blocks) {
     fprintf(stderr, "Initialize module id %d, %lu basic blocks.\n", module_id, num_basic_blocks);
 }
 
-void __csi_before_load(void *addr, int num_bytes, unsigned unused, bool unused2, bool unused3, bool read_before_write_in_bb) {
-    fprintf(stderr, "Before load %p (%d bytes) unused %d, unused2 %d, unused3 %d, read_before_write_in_bb %d\n", addr, num_bytes, unused, unused2, unused3, read_before_write_in_bb);
+void __csi_before_load(const uint64_t csi_id,
+                       const void *addr,
+                       const uint32_t num_bytes,
+                       const uint64_t prop) {
+    fprintf(stderr, "Before load %lu %p (%d bytes) prop=%lu\n",
+            csi_id, addr, num_bytes, prop);
 }
-void __csi_after_load(void *addr, int num_bytes, unsigned unused, bool unused2, bool unused3, bool read_before_write_in_bb) {
-    fprintf(stderr, "After load %p (%d bytes) unused %d, unused2 %d, unused3 %d, read_before_write_in_bb %d\n", addr, num_bytes, unused, unused2, unused3, read_before_write_in_bb);
+void __csi_after_load(const uint64_t csi_id,
+                      const void *addr,
+                      const uint32_t num_bytes,
+                      const uint64_t prop) {
+    fprintf(stderr, "After load %lu %p (%d bytes) prop=%lu\n", 
+            csi_id, addr, num_bytes, prop);
 }
-void __csi_before_store(void *addr, int num_bytes, unsigned unused, bool unused2, bool unused3, bool read_before_write_in_bb) {
-    fprintf(stderr, "Before store %p (%d bytes) unused %d, unused2 %d, unused3 %d, read_before_write_in_bb %d\n", addr, num_bytes, unused, unused2, unused3, read_before_write_in_bb);
+void __csi_before_store(const uint64_t csi_id,
+                        const void *addr,
+                        const uint32_t num_bytes,
+                        const uint64_t prop) {
+    fprintf(stderr, "Before store %lu %p (%d bytes) prop=%lu\n",
+            csi_id, addr, num_bytes, prop);
 }
-void __csi_after_store(void *addr, int num_bytes, unsigned unused, bool unused2, bool unused3, bool read_before_write_in_bb) {
-    fprintf(stderr, "After store %p (%d bytes) unused %d, unused2 %d, unused3 %d, read_before_write_in_bb %d\n", addr, num_bytes, unused, unused2, unused3, read_before_write_in_bb);
+void __csi_after_store(const uint64_t csi_id,
+                       const void *addr,
+                       const uint32_t num_bytes,
+                       const uint64_t prop) {
+    fprintf(stderr, "After store %lu %p (%d bytes) prop=%lu\n",
+            csi_id, addr, num_bytes, prop);
 }
 
-void __csi_func_entry(void *function, void *parentReturnAddr, char *funcName) {
-    fprintf(stderr, "Func entry, function %p, parent return addr %p func name %s.\n", function, parentReturnAddr, funcName);
+void __csi_func_entry(const uint64_t csi_id,
+                      const void * const function,
+                      const void * const return_addr,
+                      const char * const func_name) {
+    fprintf(stderr, "Func entry, %lu %p %p %s\n",
+            csi_id, function, return_addr, func_name);
 }
-void __csi_func_exit() {
-    fprintf(stderr, "Func exit\n");
+void __csi_func_exit(const uint64_t csi_id,
+                     const void * const function,
+                     const void * const return_addr,
+                     const char * const func_name) {
+    fprintf(stderr, "Func exit %lu %p %p %s\n",
+            csi_id, function, return_addr, func_name);
 }
-// void __csi_bb_entry(csi_id_t id) {
-void __csi_bb_entry(uint32_t module_id, uint64_t id) {
-    fprintf(stderr, "Basic block entry %d:%ld\n", module_id, id);
+void __csi_bb_entry(const uint64_t csi_id) {
+    fprintf(stderr, "Basic block entry %lu\n", csi_id);
 }
-void __csi_bb_exit() {
-    fprintf(stderr, "Basic block exit\n");
+void __csi_bb_exit(const uint64_t csi_id) {
+    fprintf(stderr, "Basic block exit %lu\n", csi_id);
 }
 
 } // extern "C"
+
+// Local Variables:
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: nil
+// End:
