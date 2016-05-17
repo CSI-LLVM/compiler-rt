df70be5ce0aeafc1064385a535700a1927f32780
Tyler Denniston <tyler@csail.mit.edu>
2016-05-17 10:15:43 -0400

    Rework csirt fed table collection structure

--- test/csi/csirt/csirt.c
+++ test/csi/csirt/csirt.c
@@ -11,50 +11,58 @@ typedef struct {
 } fed_entry;
 EXTERN_C_END
 
-static const int DEFAULT_FED_TABLE_LENGTH = 128;
+static const int DEFAULT_NUM_FED_TABLES = 128;
 static bool csi_init_called = false;
 
-// The sum of the FED size for each unit initialized. When a unit is
-// initialized, we update their fed_id_base to the current value of
-// total_fed_size, then add their FED size to total_fed_size, giving
-// each tix a globally unique set of csi_id values.
-static uint64_t total_fed_size = 0;
+typedef struct {
+    uint64_t num_entries;
+    fed_entry *entries;
+} fed_table;
 
-static uint64_t num_fed_tables = 0, fed_table_pointers_capacity = 0;
-static fed_entry **fed_table_pointers = NULL;
-static uint64_t *list_size = NULL;
+typedef struct {
+    uint64_t total_num_entries;
+    uint64_t num_fed_tables;
+    uint64_t capacity;
+    fed_table *tables;
+} fed_table_collection;
+
+static fed_table_collection fed_collection;
+static bool fed_collection_initialized = false;
 
 static inline void add_fed_table(uint64_t num_entries, fed_entry *fed_entries) {
-    if (num_fed_tables == fed_table_pointers_capacity) {
-        if (fed_table_pointers)
-            fed_table_pointers_capacity *= 2;
-        else
-            fed_table_pointers_capacity = DEFAULT_FED_TABLE_LENGTH;
+    bool need_realloc = false;
+    if (!fed_collection_initialized || fed_collection.num_fed_tables == fed_collection.capacity) {
+        if (fed_collection.tables) {
+            fed_collection.capacity *= 2;
+        } else {
+            fed_collection.capacity = DEFAULT_NUM_FED_TABLES;
+        }
+        need_realloc = true;
+        fed_collection_initialized = true;
     }
-
-    fed_table_pointers = (fed_entry **)realloc(fed_table_pointers,
-                                               fed_table_pointers_capacity * sizeof(fed_entry *));
-    list_size = (uint64_t *)realloc(list_size,
-                                    fed_table_pointers_capacity * sizeof(uint64_t));
-
-    fed_table_pointers[num_fed_tables] = fed_entries;
-    list_size[num_fed_tables] = num_entries;
-
-    num_fed_tables++;
+    if (need_realloc) {
+        fed_collection.tables = (fed_table *)realloc(fed_collection.tables,
+                                                     fed_collection.capacity * sizeof(fed_table *));
+    }
+    fed_table new_table;
+    new_table.num_entries = num_entries;
+    new_table.entries = fed_entries;
+    fed_collection.tables[fed_collection.num_fed_tables++] = new_table;
 }
 
 static inline void update_ids(uint64_t num_entries, uint64_t *fed_id_base) {
-    *fed_id_base = total_fed_size;
-    total_fed_size += num_entries;
+    *fed_id_base = fed_collection.total_num_entries;
+    fed_collection.total_num_entries += num_entries;
 }
 
 static inline fed_entry *get_fed_entry(uint64_t csi_id) {
     // TODO(ddoucet): threadsafety
     uint64_t sum = 0;
-    for (uint64_t i = 0; i < num_fed_tables; i++) {
-        if (csi_id < sum + list_size[i])
-            return &fed_table_pointers[i][csi_id - sum];
-        sum += list_size[i];
+    for (uint64_t i = 0; i < fed_collection.num_fed_tables; i++) {
+        fed_table *table = &fed_collection.tables[i];
+        if (csi_id < sum + table->num_entries)
+            return &table->entries[csi_id - sum];
+        sum += table->num_entries;
     }
 
     fprintf(stderr, "ERROR: Unable to find FED entry for csi_id %lu\n", csi_id);
