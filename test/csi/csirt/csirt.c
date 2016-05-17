f0721fbd26ceb974240e9db736edc01d05d8dae8
Tyler Denniston <tyler@csail.mit.edu>
2016-05-17 09:57:01 -0400

    Refactor csirt.c

--- test/csi/csirt/csirt.c
+++ test/csi/csirt/csirt.c
@@ -14,45 +14,46 @@ EXTERN_C_END
 static const int DEFAULT_FED_TABLE_LENGTH = 128;
 static bool csi_init_called = false;
 
-// The sum of the num_units for each unit initialized. When a unit is
-// initialized, we update their inst_id_base to the current value of
-// total_instrumentation, then add their number of instrumentations to
-// total_instrumentation, giving each tix a globally unique set of csi_id
-// values.
-static uint64_t total_instrumentation = 0;
+// The sum of the FED size for each unit initialized. When a unit is
+// initialized, we update their fed_id_base to the current value of
+// total_fed_size, then add their FED size to total_fed_size, giving
+// each tix a globally unique set of csi_id values.
+static uint64_t total_fed_size = 0;
 
-static uint64_t fed_table_length = 0, fed_table_capacity = 0;
-static fed_entry **fed_table = NULL;
+static uint64_t num_fed_tables = 0, fed_table_pointers_capacity = 0;
+static fed_entry **fed_table_pointers = NULL;
 static uint64_t *list_size = NULL;
 
-static inline void add_to_fed_table(fed_entry *entries, uint64_t num_inst) {
-    if (fed_table_length == fed_table_capacity) {
-        if (fed_table)
-            fed_table_capacity *= 2;
+static inline void add_fed_table(uint64_t num_entries, fed_entry *fed_entries) {
+    if (num_fed_tables == fed_table_pointers_capacity) {
+        if (fed_table_pointers)
+            fed_table_pointers_capacity *= 2;
         else
-            fed_table_capacity = DEFAULT_FED_TABLE_LENGTH;
+            fed_table_pointers_capacity = DEFAULT_FED_TABLE_LENGTH;
     }
 
-    fed_table = (fed_entry **)realloc(fed_table, fed_table_capacity * sizeof(fed_entry *));
-    list_size = (uint64_t *)realloc(list_size, fed_table_capacity * sizeof(uint64_t));
+    fed_table_pointers = (fed_entry **)realloc(fed_table_pointers,
+                                               fed_table_pointers_capacity * sizeof(fed_entry *));
+    list_size = (uint64_t *)realloc(list_size,
+                                    fed_table_pointers_capacity * sizeof(uint64_t));
 
-    fed_table[fed_table_length] = entries;
-    list_size[fed_table_length] = num_inst;
+    fed_table_pointers[num_fed_tables] = fed_entries;
+    list_size[num_fed_tables] = num_entries;
 
-    fed_table_length++;
+    num_fed_tables++;
 }
 
-static inline void update_ids(uint64_t num_inst, uint64_t *inst_id_base) {
-    *inst_id_base = total_instrumentation;
-    total_instrumentation += num_inst;
+static inline void update_ids(uint64_t num_entries, uint64_t *fed_id_base) {
+    *fed_id_base = total_fed_size;
+    total_fed_size += num_entries;
 }
 
 static inline fed_entry *get_fed_entry(uint64_t csi_id) {
     // TODO(ddoucet): threadsafety
     uint64_t sum = 0;
-    for (uint64_t i = 0; i < fed_table_length; i++) {
+    for (uint64_t i = 0; i < num_fed_tables; i++) {
         if (csi_id < sum + list_size[i])
-            return &fed_table[i][csi_id - sum];
+            return &fed_table_pointers[i][csi_id - sum];
         sum += list_size[i];
     }
 
@@ -63,9 +64,9 @@ static inline fed_entry *get_fed_entry(uint64_t csi_id) {
 EXTERN_C
 
 void __csirt_unit_init(const char * const name,
-                       uint64_t num_inst,
-                       uint64_t *inst_id_base,
-                       fed_entry *entries) {
+                       uint64_t num_entries,
+                       uint64_t *fed_id_base,
+                       fed_entry *fed_entries) {
     // TODO(ddoucet): threadsafety
     if (!csi_init_called) {
         // TODO(ddoucet): what to call this with?
@@ -73,9 +74,9 @@ void __csirt_unit_init(const char * const name,
         csi_init_called = true;
     }
 
-    add_to_fed_table(entries, num_inst);
-    update_ids(num_inst, inst_id_base);
-    __csi_unit_init(name, num_inst);
+    add_fed_table(num_entries, fed_entries);
+    update_ids(num_entries, fed_id_base);
+    __csi_unit_init(name, num_entries);
 }
 
 // TODO(ddoucet): why does inlining these functions cause a crash?
