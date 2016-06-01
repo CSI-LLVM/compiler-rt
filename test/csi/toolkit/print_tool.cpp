5ccb4b02bed6655ef7e9e8e37d070d0804ae7701
Tyler Denniston <tyler@csail.mit.edu>
2016-06-01 09:29:14 -0400

    Print tool now prints FED information as well

--- test/csi/toolkit/print_tool.cpp
+++ test/csi/toolkit/print_tool.cpp
@@ -1,4 +1,5 @@
 #include "csi.h"
+#include "../csirt/csirt.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -26,60 +27,85 @@ void __csi_before_load(const uint64_t csi_id,
                        const void *addr,
                        const uint32_t num_bytes,
                        const uint64_t prop) {
-    fprintf(stderr, "Before load %lu %p (%d bytes) prop=%lu\n",
-            csi_id, addr, num_bytes, prop);
+    fprintf(stderr, "Before load %lu %p (%d bytes) prop=%lu (%s:%d)\n",
+            csi_id, addr, num_bytes, prop,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_after_load(const uint64_t csi_id,
                       const void *addr,
                       const uint32_t num_bytes,
                       const uint64_t prop) {
-    fprintf(stderr, "After load %lu %p (%d bytes) prop=%lu\n",
-            csi_id, addr, num_bytes, prop);
+    fprintf(stderr, "After load %lu %p (%d bytes) prop=%lu (%s:%d)\n",
+            csi_id, addr, num_bytes, prop,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_before_store(const uint64_t csi_id,
                         const void *addr,
                         const uint32_t num_bytes,
                         const uint64_t prop) {
-    fprintf(stderr, "Before store %lu %p (%d bytes) prop=%lu\n",
-            csi_id, addr, num_bytes, prop);
+    fprintf(stderr, "Before store %lu %p (%d bytes) prop=%lu (%s:%d)\n",
+            csi_id, addr, num_bytes, prop,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_after_store(const uint64_t csi_id,
                        const void *addr,
                        const uint32_t num_bytes,
                        const uint64_t prop) {
-    fprintf(stderr, "After store %lu %p (%d bytes) prop=%lu\n",
-            csi_id, addr, num_bytes, prop);
+    fprintf(stderr, "After store %lu %p (%d bytes) prop=%lu (%s:%d)\n",
+            csi_id, addr, num_bytes, prop,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_func_entry(const uint64_t csi_id,
                       const void * const function,
                       const void * const return_addr,
                       const char * const func_name) {
-    fprintf(stderr, "Func entry, %lu %p %p %s\n",
-            csi_id, function, return_addr, func_name);
+    fprintf(stderr, "Func entry, %lu %p %p %s (%s:%d)\n",
+            csi_id, function, return_addr, func_name,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_func_exit(const uint64_t csi_id,
                      const void * const function,
                      const void * const return_addr,
                      const char * const func_name) {
-    fprintf(stderr, "Func exit %lu %p %p %s\n",
-            csi_id, function, return_addr, func_name);
+    fprintf(stderr, "Func exit %lu %p %p %s (%s:%d)\n",
+            csi_id, function, return_addr, func_name,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_bb_entry(const uint64_t csi_id) {
-    fprintf(stderr, "Basic block entry %lu\n", csi_id);
+    fprintf(stderr, "Basic block entry %lu (%s:%d)\n", csi_id,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_bb_exit(const uint64_t csi_id) {
-    fprintf(stderr, "Basic block exit %lu\n", csi_id);
+    fprintf(stderr, "Basic block exit %lu (%s:%d)\n", csi_id,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
 }
 
 void __csi_before_callsite(uint64_t csi_id, uint64_t func_id) {
-    fprintf(stderr, "Before callsite %lu to %lu\n", csi_id, func_id);
+    fprintf(stderr, "Before callsite %lu (%s:%d) to ", csi_id,
+            __csi_fed_load_get_filename(csi_id),
+            __csi_fed_load_get_line_number(csi_id));
+    if (__csirt_callsite_target_unknown(csi_id, func_id)) {
+        fprintf(stderr, "unknown function.\n");
+    } else {
+        fprintf(stderr, "%lu (%s:%d)\n", func_id,
+                __csi_fed_load_get_filename(func_id),
+                __csi_fed_load_get_line_number(func_id));
+    }
 }
 
 
