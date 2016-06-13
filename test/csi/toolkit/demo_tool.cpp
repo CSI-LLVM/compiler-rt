a9699746f5d289471f10951e288e754682b6109d
Tyler Denniston <tyler@csail.mit.edu>
2016-06-13 10:25:30 -0400

    Cleanups to demo tool

--- test/csi/toolkit/demo_tool.cpp
+++ test/csi/toolkit/demo_tool.cpp
@@ -30,17 +30,21 @@ void report() {
     delete callstack;
 }
 
-void init() {
+void init_tool() {
     called_functions = new Vector<bool>();
     callstack = new Stack<csi_id_t>();
     atexit(report);
 }
 
-void indent() {
+void print_indentation() {
     const char indention[] = "  ";
     for (int i = 0; i < indent_level; i++) {
         fprintf(stderr, indention);
     }
+}
+
+void print_counts(const instrumentation_counts_t counts) {
+    fprintf(stderr, "%lu basic blocks, %lu callsites, %lu functions, %lu function exits, %lu loads, %lu stores.", counts.num_bb, counts.num_callsite, counts.num_func, counts.num_func_exit, counts.num_load, counts.num_store);
 }
 
 }
@@ -49,62 +53,20 @@ extern "C" {
 
 void __csi_init() {
     fprintf(stderr, "Initializing the tool.\n");
-    init();
+    init_tool();
 }
 
 void __csi_unit_init(const char * const file_name,
                      const instrumentation_counts_t counts) {
-    fprintf(stderr, "Initialize unit '%s': %lu basic blocks, %lu callsites, %lu functions, %lu function exits, %lu loads, %lu stores.\n", file_name, counts.num_bb, counts.num_callsite, counts.num_func, counts.num_func_exit, counts.num_load, counts.num_store);
+    fprintf(stderr, "Initialize unit '%s'. ", file_name);
+    print_counts(counts);
+    fprintf(stderr, "\n");
     total_num_functions += counts.num_func;
     called_functions->expand(total_num_functions, false);
 }
 
-void __csi_before_load(const csi_id_t load_id,
-                       const void *addr,
-                       const int32_t num_bytes,
-                       const uint64_t prop){
-    indent();
-    fprintf(stderr, "__csi_before_load: address %p, %d bytes (%s:%d)\n",
-            addr, num_bytes,
-            __csi_fed_get_load(load_id)->filename,
-            __csi_fed_get_load(load_id)->line_number);
-}
-
-void __csi_after_load(const csi_id_t load_id,
-                      const void *addr,
-                      const int32_t num_bytes,
-                      const uint64_t prop){
-    indent();
-    fprintf(stderr, "__csi_after_load: address %p, %d bytes (%s:%d)\n",
-            addr, num_bytes,
-            __csi_fed_get_load(load_id)->filename,
-            __csi_fed_get_load(load_id)->line_number);
-}
-
-void __csi_before_store(const csi_id_t store_id,
-                        const void *addr,
-                        const int32_t num_bytes,
-                        const uint64_t prop) {
-    indent();
-    fprintf(stderr, "__csi_before_store: address %p, %d bytes (%s:%d)\n",
-            addr, num_bytes,
-            __csi_fed_get_store(store_id)->filename,
-            __csi_fed_get_store(store_id)->line_number);
-}
-
-void __csi_after_store(const csi_id_t store_id,
-                       const void *addr,
-                       const int32_t num_bytes,
-                       const uint64_t prop) {
-    indent();
-    fprintf(stderr, "__csi_after_store: address %p, %d bytes (%s:%d)\n",
-            addr, num_bytes,
-            __csi_fed_get_store(store_id)->filename,
-            __csi_fed_get_store(store_id)->line_number);
-}
-
 void __csi_func_entry(const csi_id_t func_id) {
-    indent();
+    print_indentation();
     fprintf(stderr, "__csi_func_entry: Entering function ID %ld (%s:%d)\n",
             func_id,
             __csi_fed_get_func(func_id)->filename,
@@ -117,7 +79,7 @@ void __csi_func_entry(const csi_id_t func_id) {
 void __csi_func_exit(const csi_id_t func_exit_id,
                      const csi_id_t func_id) {
     indent_level--;
-    indent();
+    print_indentation();
     fprintf(stderr, "__csi_func_exit: Exited function ID %ld (%s:%d)\n",
             func_id,
             __csi_fed_get_func_exit(func_exit_id)->filename,
@@ -126,7 +88,7 @@ void __csi_func_exit(const csi_id_t func_exit_id,
 }
 
 void __csi_bb_entry(const csi_id_t bb_id) {
-    indent();
+    print_indentation();
     fprintf(stderr, "__csi_bb_entry: %s:%d\n",
             __csi_fed_get_bb(bb_id)->filename,
             __csi_fed_get_bb(bb_id)->line_number);
@@ -135,14 +97,14 @@ void __csi_bb_entry(const csi_id_t bb_id) {
 
 void __csi_bb_exit(const csi_id_t bb_id) {
     indent_level--;
-    indent();
+    print_indentation();
     fprintf(stderr, "__csi_bb_exit: %s:%d\n",
             __csi_fed_get_bb(bb_id)->filename,
             __csi_fed_get_bb(bb_id)->line_number);
 }
-
+    
 void __csi_before_call(const csi_id_t callsite_id, const csi_id_t func_id) {
-    indent();
+    print_indentation();
     fprintf(stderr, "__csi_before_call: Calling function ID ");
     if (__csirt_is_callsite_target_unknown(callsite_id, func_id)) {
         fprintf(stderr, "<unknown> ");
@@ -158,7 +120,7 @@ void __csi_before_call(const csi_id_t callsite_id, const csi_id_t func_id) {
 }
 
 void __csi_after_call(const csi_id_t callsite_id, const csi_id_t func_id) {
-    indent();
+    print_indentation();
     fprintf(stderr, "__csi_after_call: After call to function ID ");
     if (__csirt_is_callsite_target_unknown(callsite_id, func_id)) {
         fprintf(stderr, "<unknown> ");
@@ -167,6 +129,50 @@ void __csi_after_call(const csi_id_t callsite_id, const csi_id_t func_id) {
         fprintf(stderr, "%ld ", func_id);
     }
     fprintf(stderr, "\n");
+}
+
+void __csi_before_load(const csi_id_t load_id,
+                       const void *addr,
+                       const int32_t num_bytes,
+                       const uint64_t prop){
+    print_indentation();
+    fprintf(stderr, "__csi_before_load: address %p, %d bytes (%s:%d)\n",
+            addr, num_bytes,
+            __csi_fed_get_load(load_id)->filename,
+            __csi_fed_get_load(load_id)->line_number);
+}
+
+void __csi_after_load(const csi_id_t load_id,
+                      const void *addr,
+                      const int32_t num_bytes,
+                      const uint64_t prop){
+    print_indentation();
+    fprintf(stderr, "__csi_after_load: address %p, %d bytes (%s:%d)\n",
+            addr, num_bytes,
+            __csi_fed_get_load(load_id)->filename,
+            __csi_fed_get_load(load_id)->line_number);
+}
+
+void __csi_before_store(const csi_id_t store_id,
+                        const void *addr,
+                        const int32_t num_bytes,
+                        const uint64_t prop) {
+    print_indentation();
+    fprintf(stderr, "__csi_before_store: address %p, %d bytes (%s:%d)\n",
+            addr, num_bytes,
+            __csi_fed_get_store(store_id)->filename,
+            __csi_fed_get_store(store_id)->line_number);
+}
+
+void __csi_after_store(const csi_id_t store_id,
+                       const void *addr,
+                       const int32_t num_bytes,
+                       const uint64_t prop) {
+    print_indentation();
+    fprintf(stderr, "__csi_after_store: address %p, %d bytes (%s:%d)\n",
+            addr, num_bytes,
+            __csi_fed_get_store(store_id)->filename,
+            __csi_fed_get_store(store_id)->line_number);
 }
 
 } // extern "C"
