5a8adad2eff56d5a980759dc9d7220cdab0b60be
Tyler Denniston <tyler@csail.mit.edu>
2016-06-15 16:39:13 -0400

    First CSI test passing.

--- test/csi/function-call-count.c
+++ test/csi/function-call-count.c
@@ -1,13 +1,15 @@
 // RUN: %clang_csi_toolc %tooldir/null-tool.c -o %T/null-tool.o
 // RUN: %clang_csi_toolc %tooldir/function-call-count-tool.c -o %T/tool.o
-// RUN: %link_csi %T/null-tool.o %T/tool.o -o %T/tool.o
+// RUN: %link_csi %T/tool.o %T/null-tool.o -o %T/tool.o
 // RUN: %clang_csi_c %s -o %t.o
 // RUN: %clang_csi %t.o %T/tool.o %csirtlib -o %t
-// RUN: %run %t
+// RUN: %run %t | FileCheck %s
 
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Hello world.\n");
+  printf("One call.\n");
+  printf("Two calls.\n");
+  // CHECK: num_function_calls = 2
   return 0;
 }
--- test/csi/tools/function-call-count-tool.c
+++ test/csi/tools/function-call-count-tool.c
@@ -1,6 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "csi.h"
 
+static int num_function_calls = 0;
+
+void report() {
+    printf("num_function_calls = %d\n", num_function_calls);
+}
+
 void __csi_init() {
-    fprintf(stderr, "CSI function call count tool initializing.\n");
+    num_function_calls = 0;
+    atexit(report);
+}
+
+void __csi_before_call(const csi_id_t call_id, const csi_id_t func_id) {
+    num_function_calls++;
 }
